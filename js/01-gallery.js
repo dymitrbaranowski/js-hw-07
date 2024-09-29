c46b90ea6e979e9f44a3b7b96cbe7ac74b3622ec
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-09-29 17:44:51 +0300

    fox2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+// console.log(galleryItems);
+const ulContainer = document.querySelector(".js-gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+ulContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+ulContainer.addEventListener("click", onulContainerClick);
 
-console.log(galleryItems);
+function onulContainerClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  console.log(evt.target.dataset.source);
+}
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+     <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+       />
+      </a>
+    </li>
+`;
+    })
+    .join("");
+}
+console.log(createGalleryMarkup(galleryItems));
