55fc9757a07678c6b97e46d51e925631dac792eb
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-09-30 14:50:15 +0300

    fox2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,28 +3,6 @@ import { galleryItems } from "./gallery-items.js";
 const ulContainer = document.querySelector(".gallery");
 const galleryMarkup = createGalleryMarkup(galleryItems);
 ulContainer.insertAdjacentHTML("beforeend", galleryMarkup);
-ulContainer.addEventListener("click", onUlContainerClick);
-
-function onUlContainerClick(evt) {
-  evt.preventDefault();
-  if (!evt.target.classList.contains("gallery__image")) {
-    return;
-  }
-
-  const selectedImage = evt.target.getAttribute("data-source");
-
-  const instance = basicLightbox.create(`
-         <img src="${selectedImage}" width="800" height="600">
-    `);
-
-  instance.show();
-
-  document.addEventListener("keyup", (evt) => {
-    if (evt.code === "Escape") {
-      instance.close();
-    }
-  });
-}
 
 function createGalleryMarkup(galleryItems) {
   return galleryItems
@@ -32,15 +10,17 @@ function createGalleryMarkup(galleryItems) {
       return `
       <li class="gallery__item">
        <a class="gallery__link" href="${original}">
-        <img
-          class="gallery__image"
-          src="${preview}"
-          data-source="${original}"
-          alt="${description}"
-         />
-        </a>
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
       </li>
   `;
     })
     .join("");
 }
+
+new SimpleLightbox(".gallery a", {
+  animationSpeed: 250,
+  overlayOpacity: 0.7,
+  captionsData: "alt",
+  captionDelay: 250,
+});
